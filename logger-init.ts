6224863a28ebab0bb788fa73728372c7d726615b
Matthewwwss <kukulesafroditov@gmail.com>
2025-05-29 13:04:25 +0300

    Delete logger-init.ts

--- logger-init.ts
+++
@@ -1,9 +0,0 @@
-import logger from "./utils/logger";
-
-if (import.meta.env.MODE === "development") {
-  logger.setLevel("debug");
-} else {
-  logger.setLevel("error");
-}
-
-export default logger;
